a936d3187acc0779c3420fbc14960b4842235996
dev-kaustav <job.kaustav@gmail.com>
2023-02-04 00:27:02 +0530

    added cpp starter files

---
+++ .vscode/better-comments.cpp
@@ -0,0 +1,16 @@
+// normal comment
+// // crossed comment
+
+// * more visible
+
+// _specify sections
+
+// ! errors
+
+// ? what's happenning?
+
+// TODO tasks
+// // taask 1
+// # task 2
+// # task 3
+// # task 4
